e11922ff42aed9beaee8b203d19054bb2e8ec9e3
kawanet <u-suke@kawa.net>
2020-05-17 17:21:39 +0900

    $$ to $

--- test/escape.test.ts
+++ test/escape.test.ts
@@ -13,4 +13,18 @@ describe(TITLE, () => {
             assert.equal(replacer("[X]"), "[\r:\n:\t:\\:\x41:\u0061]");
         });
     }
+    {
+        const def = "s/(X)/$1:$2:$$/";
+        it(def, () => {
+            const replacer = sed(def);
+            assert.equal(replacer("[X]"), "[X:$2:$]");
+        });
+    }
+    {
+        const def = "s/(X)/\\$1:\\x241:\\u00241/";
+        it(def, () => {
+            const replacer = sed(def);
+            assert.equal(replacer("[X]"), "[X:X:X]");
+        });
+    }
 });
